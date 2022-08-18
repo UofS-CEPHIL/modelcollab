29a037287afd8b32d563dd9413a9d81587f3652d
Eric Redekopp <eric.redekopp@usask.ca>
2022-08-17 19:13:43 -0600

    Got docker container deployed to Cloud Run and functional for .png results

--- server/src/main/ts/ComputeModelTask.ts
+++ server/src/main/ts/ComputeModelTask.ts
@@ -17,20 +17,31 @@ export default class ComputeModelTask {
 
     public async start(onResultsReady?: (path: string) => void): Promise<void> {
         const date: string = new Date().toISOString().slice(0, 16);
-        const filename: string = `./ModelResults_${date}`;
-        const juliaCode: string = new JuliaGenerator(JuliaComponentDataBuilder.makeJuliaComponents(this.components)).generateJulia(filename);
-        console.log(juliaCode.split(';'));
-        let proc = spawn(
-            "julia",
-            {
-                stdio: ["pipe", "inherit", "inherit"],
-                cwd: applicationConfig.algebraicStockFlowFilePath
-            }
-        );
-        proc.stdin.write(juliaCode);
-        proc.stdin.write("\n");
-        proc.stdin.write("exit()\n");
-        proc.stdin.end();
-        if (onResultsReady) proc.on("exit", () => onResultsReady(filename));
+        const filename: string = `/tmp/ModelResults_${date}`;
+        try {
+            const juliaCode: string = new JuliaGenerator(JuliaComponentDataBuilder.makeJuliaComponents(this.components)).generateJulia(filename);
+            console.log(juliaCode.split(';'));
+            let proc = spawn(
+                "julia",
+                {
+                    stdio: ["pipe", "inherit", "inherit"],
+                    cwd: "/home/mc"
+                }
+            );
+            proc.stdin.write('ENV["GKSwstype"] = "nul"\n');
+            proc.stdin.write(juliaCode);
+            proc.stdin.write("\n");
+            proc.stdin.write("exit()\n");
+            proc.stdin.end();
+            if (onResultsReady)
+                proc.on("exit", (code) => {
+                    console.log("Process exited with code " + code);
+                    onResultsReady(filename + ".png");
+                });
+        }
+        catch (e) {
+            console.error(e);
+            if (onResultsReady) onResultsReady(filename);
+        }
     }
 }
--- server/src/main/ts/config/applicationConfig.ts
+++ server/src/main/ts/config/applicationConfig.ts
@@ -1,6 +1,9 @@
 const applicationConfig = {
     port: 8080,
-    algebraicStockFlowFilePath: "/home/eric/cephil/code/model-collab/modelcollab/server/src/main/julia/"
+    algebraicStockFlowFilePath: "/home/eric/cephil/code/model-collab/modelcollab/server/src/main/julia/",
+
+    juliaPath: "/home/mc/modelcollab/server/julia-1.7.3/bin/julia",
+    //juliaPath: "/bin/julia",
 }
 
 export default applicationConfig;
