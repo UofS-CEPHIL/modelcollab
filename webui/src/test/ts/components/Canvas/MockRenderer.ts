4869c15e6b504401708540427e98b84cd924174d
Eric Redekopp <eric.redekopp@usask.ca>
2022-11-14 17:29:18 -0600

    delete tests for production

--- webui/src/test/ts/components/Canvas/MockRenderer.ts
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from "react";
-import ComponentRenderer from "../../../../main/ts/components/Canvas/Renderer/ComponentRenderer";
-
-export default class MockRenderer implements ComponentRenderer {
-
-    public readonly render: jest.Mock<ReactElement[]> = jest.fn();
-
-}
