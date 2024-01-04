4e8d445b07d2789aaf6150149bf8eca0978d3a7c
Eric Redekopp <eric.redekopp@usask.ca>
2024-01-04 09:33:12 -0600

    Marginally improve login screen (proper landing page still needed)

--- webui/src/main/ts/view/Screens/LoginScreen.tsx
+++ webui/src/main/ts/view/Screens/LoginScreen.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect } from 'react';
+import { Button, Typography } from '@mui/material';
+import React, { Fragment, ReactElement } from 'react';
 
 import FirebaseManager from "../../data/FirebaseManager";
 
@@ -6,21 +7,36 @@ interface Props {
     firebaseManager: FirebaseManager;
 };
 
-const LoginScreen: FC<any> = (props: Props) => {
+export default class LoginScreen extends React.Component<Props> {
+    public render(): ReactElement {
+        return (
+            <Fragment>
+                <Typography variant="h3">
+                    Welcome to ModelCollab!
+                </Typography>
+                <Typography sx={{ maxWidth: "800px" }}>
+                    ModelCollab is a brand new application for system dynamics
+                    modelling with an innovative collaborative user interface for
+                    group modelling over the web. This application is still under
+                    construction and may still have bugs and/or lack features.
+                    To request a feature, report a bug, or for more information,
+                    see our&nbsp;
+                    <a href="https://github.com/UofS-CEPHIL/modelcollab">
+                        Github page
+                    </a>.
+                    All authentication is handled through Google:
+                    click the button below to log in!
+                </Typography>
 
-    useEffect(() => { document.title = "ModelCollab" }, []);
+                <Button
+                    onClick={() => this.props.firebaseManager.login()}
+                    variant="contained"
+                    id="login-button"
+                >
+                    Sign in with Google
+                </Button>
+            </Fragment>
+        );
+    }
 
-    const onClickLoginButton = () => {
-        props.firebaseManager.login();
-    };
-
-    return (
-        <div>
-            <button onClick={onClickLoginButton} id="login-button">
-                Sign in with Google
-            </button>
-        </div>
-    )
 }
-
-export default LoginScreen;
