573789fb430f6c92bb5a3ae6205791f96ef3d0b7
Eric Redekopp <eric.redekopp@usask.ca>
2024-04-06 14:57:46 -0600

    Hide firebase API keys in webui (oops)

--- webui/src/main/ts/config/applicationConfig.dist.ts
+++ webui/src/main/ts/config/applicationConfig.dist.ts
@@ -3,8 +3,7 @@ const applicationConfig = {
     appName: "ModelCollab",
 
     // The address of the backend server
-    serverAddress: "https://35.225.239.184:80",
-    //serverAddress: "http://127.0.0.1:8088",
+    serverAddress: "",
 };
 
 export default applicationConfig;
---
+++ webui/src/main/ts/config/firebaseConfig.dist.ts
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    useEmulators: false
+};
+
+export default firebaseConfig;
